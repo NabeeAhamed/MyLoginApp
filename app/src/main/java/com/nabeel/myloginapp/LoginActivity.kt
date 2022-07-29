7fd0dd2141a948a0b1c948aed2ae381106d4d5ed
NabeeAhamed <nk1776594@gmail.com>
2022-07-29 17:22:14 +0530

    Add Data Pass
    Data Passing with one Activity to Another

--- app/src/main/java/com/nabeel/myloginapp/LoginActivity.kt
+++ app/src/main/java/com/nabeel/myloginapp/LoginActivity.kt
@@ -4,11 +4,10 @@ package com.nabeel.myloginapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.widget.doOnTextChanged
-import java.util.jar.Attributes
+
 
 class LoginActivity : AppCompatActivity() {
     lateinit var btnOpen:Button
@@ -32,44 +31,63 @@ class LoginActivity : AppCompatActivity() {
             val Name = etName.text.toString()
             val LastName = etLastName.text.toString()
             val Age = etAge.text.toString()
-            val City =etCity.text.toString()
-            val Phone =etPhone.text.toString()
+            val City = etCity.text.toString()
+            val Phone = etPhone.text.toString()
 
 
-            if(Name.isNullOrBlank()){
+            if (Name.isNullOrBlank()) {
                 etName.error = resources.getString(R.string.enter_a_valid_name)
                 etName.requestFocus()
-            }else if(LastName.isNullOrEmpty()){
+            } else if (LastName.isNullOrEmpty()) {
                 etLastName.error = resources.getString(R.string.enter_a_valid_Last_Name)
                 etLastName.requestFocus()
-            }else  if(Age.isNullOrEmpty()) {
+            } else if (Age.isNullOrEmpty()) {
                 etAge.error = resources.getString(R.string.enter_a_valid_Age)
                 etAge.requestFocus()
-            }else  if(City.isNullOrEmpty()) {
+            } else if (City.isNullOrEmpty()) {
                 etCity.error = resources.getString(R.string.enter_a_valid_City)
                 etCity.requestFocus()
-            }else if (Phone.isNullOrEmpty()) {
+            } else if (Phone.isNullOrEmpty()) {
                 etPhone.error = resources.getString(R.string.enter_a_valid_Phone)
                 etPhone.requestFocus()
-            }else{
+            } else {
 
-            val intent = Intent(this@LoginActivity,Loginactivity3::class.java)
-            intent.putExtra("name",Name)
-            intent.putExtra("lastName",LastName)
-            intent.putExtra("Phone",Integer.valueOf(etPhone.text.toString()))
-           intent.putExtra("Age",Integer.valueOf(etAge.text.toString()))
-           intent.putExtra("City",etCity.text.toString())
-            startActivity(intent)}
-
+                val intent = Intent(this@LoginActivity, Loginactivity3::class.java)
+                intent.putExtra("name", Name)
+                intent.putExtra("lastName", LastName)
+                intent.putExtra("Phone", Integer.valueOf(etPhone.text.toString()))
+                intent.putExtra("Age", Integer.valueOf(etAge.text.toString()))
+                intent.putExtra("City", etCity.text.toString())
+                startActivity(intent)
+            }
 
         }
-
 
         etName.doOnTextChanged { text, _, _, _ ->
             if ((text?.length ?: 0) < 10) {
                 etName.error = resources.getString(R.string.enter_a_valid_name)
 
         }
+           }
+            etLastName.doOnTextChanged { text, _, _, _ ->
+                if ((text?.length ?: 0) < 10) {
+                    etLastName.error = resources.getString(R.string.enter_a_valid_Last_Name)
         }
-    }
-}
+            }
+                etAge.doOnTextChanged { text, _, _, _ ->
+                    if ((text?.length ?: 0) < 10) {
+                        etAge.error = resources.getString(R.string.enter_a_valid_Age)
+                    }
+                }
+                    etCity.doOnTextChanged { text, _, _, _ ->
+                        if ((text?.length ?: 0) < 10) {
+                            etCity.error = resources.getString(R.string.enter_a_valid_City)
+                        }
+                    }
+                        etPhone.doOnTextChanged { text, _, _, _ ->
+                            if ((text?.length ?: 0) < 10) {
+                                etPhone.error = resources.getString(R.string.enter_a_valid_Phone)
+                            }
+                        }
+    }}
+
